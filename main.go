d6f0e8e406064dde3594fc4dd138bb92f2d3c5db
Robert Gleason <l2ob122@gmail.com>
2024-05-22 18:45:31 -0700

    Refactor main.go to use a path-based handler

--- main.go
+++ main.go
@@ -5,19 +5,29 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	fmt.Fprint(w, "<h1>Epiq lens</h1")
 }
 
-func contactHandler(w http.ResponseWriter, r *http.Request) {
+func contactHandler(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch with me email me at <a href=\"mailto:epiqlens@gmail.com\">epiqlens@gmail.com</a>.</p>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		// TODO: Handle page not found error
+	}
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting server on :3000")
 	http.ListenAndServe(":3000", nil)
 
