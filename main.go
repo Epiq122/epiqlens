3904dd5d7faf98912116bf6abf5c1647e3f8887a
Robert Gleason <l2ob122@gmail.com>
2024-05-22 19:56:46 -0700

    chore: Update FAQ page handler and templates

--- main.go
+++ main.go
@@ -38,24 +38,7 @@ func contactHandler(w http.ResponseWriter, _ *http.Request) {
 }
 
 func faqHandler(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, `<h1>FAQ Page</h1>
-	<ul>
-		<li>
-			<b>Is there a free version?</b>
-			Yes! We offer a free trial for 30 days on any paid plans.
-		</li>
-		<li>
-			<b>What are your support hours?</b>
-			We have support staff answering emails 24/7, though response
-			times may be a bit slower on weekends.
-		</li>
-		<li>
-			<b>How do I contact support?</b>
-			Email us - <a href=\"mailto:epiqlens@gmail.com\">epiqlens@gmail.com</a>
-		</li>
-	</ul>
-	`)
+	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
 }
 
 func main() {
