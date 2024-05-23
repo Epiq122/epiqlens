af95763f6d71c0d5835ce9caf3a6b10e3b02d859
Robert Gleason <l2ob122@gmail.com>
2024-05-23 16:39:53 -0700

    chore: Update FAQ page handler and templates

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/epiq122/epiqlens/views"
@@ -17,5 +18,32 @@ func (static Static) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "Is there going to be a free version?",
+			Answer:   "Yes! We will be offering a 30 day free trial",
+		},
+		{
+			Question: "What are your support hours?",
+			Answer:   "These are TBD",
+		},
+		{
+			Question: "How do I contact support?",
+			Answer:   `Email us - <a href="mailto:epiqlens@gmail.com">epiqlens@gmail.com</a>`,
+		},
+		{
+			Question: "When is the release?",
+			Answer:   "Christmas 2024",
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }
